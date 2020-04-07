924d97172b8d9aa5dc8da667e760b09f5a793780
jaren <jarensmart@yahoo.com>
2020-04-06 20:25:45 -0400

    styled and received data

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -4,31 +4,31 @@ import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
   const [characters, setCharacters] = useState([]);
-  console.log(characters);
 
   useEffect(() => {
     Axios.get(
       "https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/"
     )
-      .then(response => {
+      .then((response) => {
         // console.log("This is your response: ", response.data.results);
         setCharacters(response.data.results);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log("You have an error: ", error);
       });
   }, []);
 
   return (
     <section className="character-list">
-      {characters.map(props => {
+      {characters.map((char) => {
         return (
           <CharacterCard
-            name={props.name}
-            image={props.image}
-            status={props.status}
-            species={props.species}
-            gender={props.gender}
+            key={char.name}
+            name={char.name}
+            image={char.image}
+            status={char.status}
+            species={char.species}
+            gender={char.gender}
           />
         );
       })}
